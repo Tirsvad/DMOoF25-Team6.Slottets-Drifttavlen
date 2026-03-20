320808ed93d4c8ecfca21a88d0c96ca66af1edcb
William <wbch62382@edu.ucl.dk>
2026-03-20 19:18:24 +0100

    Implemented UC002-DashBoard-ResidentNote-Code-API

--- src/Infrastructure/Repositories/ResidentNoteRepository.cs
+++ src/Infrastructure/Repositories/ResidentNoteRepository.cs
@@ -5,6 +5,10 @@ using Core.Interfaces.Repositories;
 
 using Domain.Entities;
 
+using Infrastructure.Persistents;
+
+using Microsoft.EntityFrameworkCore;
+
 namespace Infrastructure.Repositories;
 
 /// <summary>
@@ -21,6 +25,29 @@ namespace Infrastructure.Repositories;
 /// var notes = repository.GetAll();
 /// </code>
 /// </example>
+/// 
+
+
+
 public class ResidentNoteRepository : Repository<ResidentNote>, IResidentNoteRepository
 {
+
+    private readonly AppDbContext _context;
+
+    //The constructor receives AppDbContext through dependency injection
+    public ResidentNoteRepository(AppDbContext context) 
+    {
+        _context = context;
+    }
+
+
+    public async Task<IEnumerable<ResidentNote>> GetAllbyResidentAsync()
+    {
+        // get all resident notes with their associated resident. 
+        return await _context.ResidentNotes.Include (n=> n.Resident).ToListAsync();
+
+    }
+
 }
+
+
