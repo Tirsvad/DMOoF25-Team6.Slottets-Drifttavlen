141ea0ae911b7b1c2576e3c8031fa441eb1503b6
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2026-04-07 17:37:00 +0800

    add tests

---
+++ tests/Infrastructure.Tests/DummyTests.cs
@@ -0,0 +1,16 @@
+// Copyright (c) 2026 Team6. All rights reserved. 
+//  No warranty, explicit or implicit, provided.
+
+namespace Infrastructure.Tests;
+
+public class DummyTests
+{
+    [Fact]
+    public void DummyTest()
+    {
+        // Arrange
+        // Act
+        // Assert
+        Assert.True(true);
+    }
+}
