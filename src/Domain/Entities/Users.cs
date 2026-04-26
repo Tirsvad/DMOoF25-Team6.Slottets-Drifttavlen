abe8d26da7af6c82e1d0a9f8e26c409963101f12
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2026-04-27 04:24:39 +0800

    Update database

--- src/Domain/Entities/Users.cs
+++ src/Domain/Entities/Users.cs
@@ -1,6 +1,8 @@
 // Copyright (c) 2026 Team6. All rights reserved. 
 //  No warranty, explicit or implicit, provided.
 
+using System.ComponentModel.DataAnnotations;
+
 using Domain.Interfaces;
 
 using Microsoft.AspNetCore.Identity;
@@ -9,4 +11,16 @@ namespace Domain.Entities;
 
 public class User : IdentityUser<Guid>, IEntity
 {
+    /// <summary>
+    /// Gets or sets the user name for this user. Always stored in lowercase for consistency.
+    /// </summary>
+    [ProtectedPersonalData]
+    [EmailAddress]
+    public override string? UserName
+    {
+        get => _userName;
+        set => _userName = value?.ToLowerInvariant();
+    }
+
+    private string? _userName;
 }
